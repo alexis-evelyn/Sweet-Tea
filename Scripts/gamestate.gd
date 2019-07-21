25d1806d384ff32aca980dc9eff3a97cfbd37e24
Alex Evelyn <github@senorcontento.com>
2019-07-20 21:01:50 -0400

    Changing Default Player ID to 1. Keeps Debugger Happy and Allows Server to Register Itself

--- Scripts/gamestate.gd
+++ Scripts/gamestate.gd
@@ -3,7 +3,7 @@ extends Node
 # Player Info Dictionary
 var player_info = {
 	name = "Player", # Player's Name
-	net_id = -1, # Player's ID
+	net_id = 1, # Player's ID
 	actor_path = "res://Objects/Players.tscn", # The Player's Scene (Comparable to Class)
 	char_color = Color(1, 1, 1) # Unmodified Player Color - May Combine With Custom Sprites (and JSON)
 }
