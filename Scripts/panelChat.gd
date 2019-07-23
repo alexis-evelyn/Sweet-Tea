65f38ef250a11342c6a3e6bbdae1dc4fd1eb558b
Alex Evelyn <github@senorcontento.com>
2019-07-22 21:39:10 -0400

    The RichTextLabel was easier to setup than what I was previously doing with a VBox and Labels. Plus it solves the autoscroll issue and sets the chat up for more complex formatting (e.g. colors and custom fonts)

--- Scripts/panelChat.gd
+++ Scripts/panelChat.gd
@@ -1,13 +1,15 @@
 extends Panel
 
 # Declare member variables here. Examples:
-var max_lines = 500
+onready var chatMessages = $chatMessages
+onready var chat = $userChat
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var chat = $userChat
 	chat.add_font_override("font", load("res://Fonts/dynamicfont/firacode-regular.tres")) # TODO: Fonts will be able to be chosen by player (including custom fonts added by player)
-	# $ScrollContainer.set_scroll_follow(true) # Why doesn't this exist for ScrollContainer?
+	chatMessages.set("custom_fonts/normal_font", load("res://Fonts/dynamicfont/firacode-regular.tres")) # TODO: Fonts will be able to be chosen by player (including custom fonts added by player)
+	
+	chatMessages.set_scroll_follow(true) # Sets RichTextLabel to AutoScroll if at Bottom
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -17,21 +19,7 @@ func _ready():
 sync func chat_message_client(message):
 	print("Client Message: ", message)
 	
-	var chatMessages = $ScrollContainer/chatMessages
-	var chatMessage = Label.new()
-	chatMessage.text = message
-	chatMessage.add_font_override("font", load("res://Fonts/dynamicfont/firacode-regular.tres")) # TODO: Fonts will be able to be chosen by player (including custom fonts added by player)
-	
-	if(chatMessages.get_child_count() >= max_lines):
-		chatMessages.remove_child(chatMessages.get_child(0))
-	
-	chatMessages.add_child(chatMessage)
-	
-	print("V-Scrollbar Position: ", $ScrollContainer.get_v_scroll()) # Current Scroll Position (Starts at 0)
-	
-	# Pseudo-Code explained in scroll_started/ended
-	# Psuedo-Code: if scrollbar.auto = true
-		#$ScrollContainer.set_v_scroll($ScrollContainer.MAX_SCROLL)
+	chatMessages.add_text(message + "\n")
 	
 # Process Chat Message from Client
 master func chat_message_server(message):
@@ -53,13 +41,3 @@ func _on_userChat_gui_input(event):
 			print("Enter Key Pressed!!!")
 			rpc_id(1, "chat_message_server", chat.text)
 			chat.text = ""
-
-# TODO: I am trying to figure out how to detect if the user manually scrolled to the bottom. If the user did scroll to the bottom, autoscroll should be enabled. Otherwise if the user scrolls up, autoscroll turns off.
-# What are deadzones?
-func _on_ScrollContainer_scroll_ended():
-	# I cannot get this to be called. How do I activate it?
-	print("Scroll Ended")
-
-func _on_ScrollContainer_scroll_started():
-	# I cannot get this to be called. How do I activate it?
-	print("Scroll Started")
