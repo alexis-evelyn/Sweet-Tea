210f6ca713795091d65900df93629047781164d6
Alex Evelyn <github@senorcontento.com>
2019-08-05 02:24:09 -0400

    Adding More Comments About Compiling Templates

--- Scripts/Menu/main_menu.gd
+++ Scripts/Menu/main_menu.gd
@@ -22,6 +22,10 @@ extends Control
 # https://docs.godotengine.org/en/3.1/development/compiling/compiling_for_x11.html#building-export-templates
 # OSX does not have explicit instructions on how to compile export templates - http://docs.godotengine.org/en/stable/development/compiling/compiling_for_osx.html
 
+# Since I Use Mac, I Am Leaving A Link About Cross Compiling Godot
+# http://docs.godotengine.org/en/stable/development/compiling/compiling_for_windows.html#cross-compiling-for-windows-from-other-operating-systems
+# It appears I cannot cross compile for Linux from Mac, so I will have to install a Linux VM when I get close to releasing the game
+
 # I am currently compiling for OSX with the command: scons platform=osx tools=no target=release bits=64
 # Lets Hope it Works
 func test_module():
