72e2b93f50fcac7df8951ea63fe678be0c22dc95
Alex Evelyn <gpg@senorcontento.com>
2019-09-20 02:54:19 -0400

    Semi-Functional Hotbits

---
+++ Custom/cacert/ReadMe-CACert.md
@@ -0,0 +1,7 @@
+The ca certificate file was downloaded from:
+* https://curl.haxx.se/ca/cacert.pem
+
+The website to click the link is:
+* https://curl.haxx.se/docs/caextract.html
+
+This certificate came from the certificate that is used by Firefox. It was converted to be useful for the Curl command.
--- Scripts/functions/hotbits.gd
+++ Scripts/functions/hotbits.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name Hotbits
 
 # Pulls Random Data From Geiger Counter At Fourmilabs. Can be used to retrieve truly random data in the quantum sense.
 # Take that determinism!!! :P
@@ -12,16 +13,87 @@ extends Node
 
 # Declare member variables here. Examples:
 var api_url : String = "https://www.fourmilab.ch/cgi-bin/Hotbits.api" # API Url
-var test_key : String = "pseudo" # Useful for testing without rate limiting or banning
+var test_key : String = "Pseudorandom" # Useful for testing without rate limiting or banning
 var data_format : String = "json" # Available Formats: hex, bin, c, xml, json, password
 var number_of_requested_bytes : int = 2048 # Maximum 2048 bytes - Not Active When format is password
 
+var number_of_passwords : int = 0
+var length_of_passwords : int = 0
+var requested_password_type : int = password_type.letters_numbers_and_punctuation
+
+# Apparently Hotbits No Longer Supports Post Requests :(
+var request_method : int = HTTPClient.METHOD_GET
+
+var user_agent : String = functions.get_translation(ProjectSettings.get_setting("application/config/name"), "en")
+var validate_domain : bool = true # Validate Domain if Using HTTPS.
+
 var real_key : String # Do Not Store Real Key in Code - Key To Be Requested From This Form: https://www.fourmilab.ch/hotbits/apikey_request.html
+
+var headers : Array = [
+	"user-agent: %s" % [user_agent],
+	"Content-type: application/x-www-form-urlencoded"
+]
+
+var query : Dictionary = {
+	"nbytes": number_of_requested_bytes,
+	"fmt": data_format,
+	"apikey": get_api_key(),
+
+	"pwtype": requested_password_type,
+	"npass": number_of_passwords,
+	"lpass": length_of_passwords
+}
+
+enum password_type {
+	lower_case_letters = 0,
+	mixed_case_letters = 1,
+	letters_and_numbers = 2,
+	letters_numbers_and_punctuation = 3
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	request_data()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func request_data():
+	$HTTPRequest.request(api_url + to_query_string(query), headers, validate_domain, request_method)
+
+func request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray):
+#	logger.verbose("Hotbits Result Code: %s" % result)
+#	logger.verbose("Hotbits Response Code: %s" % response_code)
+#
+#	for header in headers:
+#		logger.verbose("Hotbits Header: %s" % header)
+
+	logger.warn("Body: %s" % body.get_string_from_utf8())
+
+	if data_format == "json":
+		# Run Function to Parse As Json (After Checking To Make Sure Not HTML Rate Limit Message)
+		pass
+	else:
+		# Unrecognized Format, Print To Logger and stdout
+		logger.warn("Format: %s\nBody: %s" % [data_format, body.get_string_from_utf8()])
+
+func get_api_key() -> String:
+	if real_key.empty():
+		return test_key
+
+	return real_key
+
+func to_query_string(dictionary: Dictionary) -> String:
+	var query_string : String = "?"
+
+	for key in dictionary.keys():
+		query_string += "%s=%s&" % [key, dictionary[key]]
+
+	query_string = query_string.rstrip("&") # Remove last Ampersand
+
+	return query_string
+
+func get_class() -> String:
+	return "Hotbits"
