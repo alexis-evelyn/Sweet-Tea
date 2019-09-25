e349056439dc67ddc9e2a0acb37592d4c9715c13
Alex Evelyn <gpg@senorcontento.com>
2019-09-25 06:00:53 -0400

    Working on Dictionary. Welcome to Insomnia Again :(

--- Scripts/functions/dictionary_registry.gd
+++ Scripts/functions/dictionary_registry.gd
@@ -17,45 +17,78 @@ var mazawalza : Dictionary = {
 	# Tag (Dictionary Key), Unicode Representation, Meaning, Modifier, Etc...
 	# Don't Access Dictionary Directly. Use Getter Function to Fill in Missing Info For Current Locale
 	"language_name": {
-
+		"character": "\udb80",
+		"entry": "entry_language_name",
+		"meaning": "meaning_language_name"
 	},
 	"language_name_closed": {
-
+		"character": "\udb81",
+		"entry": "entry_language_name_closed",
+		"meaning": "meaning_language_name_closed"
 	},
 	"rs": {
-
+		"character": "\udb82",
+		"entry": "entry_",
+		"meaning": "meaning_"
 	},
 	"rsc": {
-
+		"character": "\udb83",
+		"entry": "entry_",
+		"meaning": "meaning_"
 	},
 	"s": {
-
+		"character": "\udb84",
+		"entry": "entry_",
+		"meaning": "meaning_"
 	},
 	"sc": {
-
+		"character": "\udb85",
+		"entry": "entry_",
+		"meaning": "meaning_"
 	},
 	"camera": {
-
+		"character": "\udb86",
+		"entry": "entry_camera",
+		"meaning": "meaning_camera"
 	},
 	"camera_closed": {
-
+		"character": "\udb87",
+		"entry": "entry_camera_closed",
+		"meaning": "meaning_camera_closed"
 	},
 	"effect_strength": {
-
+		"character": "\udb88",
+		"entry": "entry_effect_strength",
+		"meaning": "meaning_effect_strength",
+		"modifier": modifier.strength
 	},
 	"effect_weakness": {
-
+		"character": "\udb89",
+		"entry": "entry_effect_weakness",
+		"meaning": "meaning_effect_weakness",
+		"modifier": modifier.weakness
 	},
 	"eye": {
-
+		"character": "\udb8a",
+		"entry": "entry_eye",
+		"meaning": "meaning_eye"
 	},
 	"eye_closed": {
-
+		"character": "\udb8b",
+		"entry": "entry_eye_closed",
+		"meaning": "meaning_eye_closed"
 	}
 }
 
+# May Use. May Not
 var entry : String = "entry_"
 var meaning : String = "meaning_"
+
+# Effect Modifiers
+enum modifier {
+	strength = 0,
+	weakness = 1
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
