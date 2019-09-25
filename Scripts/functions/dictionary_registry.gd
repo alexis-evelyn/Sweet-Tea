3e5b7f3f9bf2e50eec8c8de44944a25581f05654
Alex Evelyn <gpg@senorcontento.com>
2019-09-25 03:26:08 -0400

    Started Work on Dictionary

---
+++ Scripts/functions/dictionary_registry.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# This is specifically for Mazawalza!!! Nothing Else!!!
+
+# Links
+# https://conlang.stackexchange.com/a/1031/1369
+# https://en.wikipedia.org/w/index.php?title=Blissymbols&oldid=915905921#Examples
+
+# What Is Mazawalza
+# Mazawalza is a magic based language for the lore of my game.
+# As such, the concepts behind the characters will be geared toward spells and potions.
+# Unlike Mincraft's random symbols with enchanting tables,
+# I want these symbols to have meaning so that someone who puts the effort to learn to read it can understand what the spells and potions do.
+
+# Declare member variables here. Examples:
+var mazawalza : Dictionary = {
+# Tag (Dictionary Key), Unicode Representation, Meaning, Modifier, Etc...
+# Don't Access Dictionary Directly. Use Getter Function to Fill in Missing Info For Current Locale
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
