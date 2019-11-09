c0388221c9c37601f6d301372e02d505eaea7d31
Alex Evelyn <gpg@senorcontento.com>
2019-11-08 23:47:28 -0500

    Added Print Functions for EICAR and GTube

--- Scripts/Lua/virus_scanner.gd
+++ Scripts/Lua/virus_scanner.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name Vaccine # It's a joke, but it really is like having a vaccine.
 
 # I MAY IMPLEMENT A VIRUS SCANNER FOR THE VIRTUAL COMPUTERS
 # Use https://www.virustotal.com/gui/home/upload to test the EICAR Test Virus (NOT A REAL VIRUS, BUT SHOULD TRIGGER VIRUS SCANNER ANYWAY)!!!
@@ -13,18 +14,30 @@ var enabled = true # Enable Virus And Spam Scanner
 
 # EICAR Explanation Wikipedia - https://en.wikipedia.org/wiki/EICAR_test_file
 # EICAR Explanation (Developer's Website) - http://2016.eicar.org/86-0-Intended-use.html
-var eicar_part_one = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-"
-var eicar_part_two = "STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" # EICAR Test File - ¡¡¡NOT A REAL VIRUS!!!
+const eicar_part_one = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-"
+const eicar_part_two = "STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" # EICAR Test File - ¡¡¡NOT A REAL VIRUS!!!
 
 # GTube Explanation Wikipedia - https://en.wikipedia.org/wiki/GTUBE
 # GTube Explanation (Developer's Website) - https://spamassassin.apache.org/gtube/
-var gtube_part_one = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE"
-var gtube_part_two = "-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" # GTube Test Line - ¡¡¡NOT REAL SPAM!!!
+const gtube_part_one = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE"
+const gtube_part_two = "-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" # GTube Test Line - ¡¡¡NOT REAL SPAM!!!
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_process(false)
+
+	print_eicar() # Print EICAR Test Virus
+	print_gtube() # Print GTube Test Spam
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
+
+func print_eicar() -> void:
+	logger.superverbose("EICAR (Test Anti-Malware): %s%s" % [eicar_part_one, eicar_part_two])
+
+func print_gtube() -> void:
+	logger.superverbose("GTube (Test Spam Filter): %s%s" % [gtube_part_one, gtube_part_two])
+
+func get_class() -> String:
+	return "Vaccine"
