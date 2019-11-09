ffa483ce210c959b491dfc752cee764c49d92ccd
Alex Evelyn <gpg@senorcontento.com>
2019-11-08 23:32:14 -0500

    Splitting Test EICAR String

--- Scripts/Lua/virus_scanner.gd
+++ Scripts/Lua/virus_scanner.gd
@@ -7,13 +7,19 @@ extends Node
 var enabled = true # Enable Virus And Spam Scanner
 
 # Because EICAR is Escaped, It Won't Trigger Your Virus Scanner turn 4\\P into 4\P to Trigger Virus Scanner.
+# Here's the interesting part, when testing the compiled version on virustotal.com, only five scanners picked up the EICAR string.
+# So, most scanners don't work when I just straight up put the string in the file. I split the EICAR test string into two variables
+# to prevent a host virus scanner from detecting it (causing my game to look like a virus) as opposed to my in game scanner using it for a test.
+
 # EICAR Explanation Wikipedia - https://en.wikipedia.org/wiki/EICAR_test_file
 # EICAR Explanation (Developer's Website) - http://2016.eicar.org/86-0-Intended-use.html
-var eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" # EICAR Test File - ¡¡¡NOT A REAL VIRUS!!!
+var eicar_part_one = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-"
+var eicar_part_two = "STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" # EICAR Test File - ¡¡¡NOT A REAL VIRUS!!!
 
 # GTube Explanation Wikipedia - https://en.wikipedia.org/wiki/GTUBE
 # GTube Explanation (Developer's Website) - https://spamassassin.apache.org/gtube/
-var gtube = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" # GTube Test Line - ¡¡¡NOT REAL SPAM!!!
+var gtube_part_one = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE"
+var gtube_part_two = "-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" # GTube Test Line - ¡¡¡NOT REAL SPAM!!!
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
