c9d10191980cd95b09b279a4e32cbc1c92a13588
Alex Evelyn <gpg@senorcontento.com>
2019-11-08 23:16:24 -0500

    Added Test Virus and Test Spam

---
+++ Scripts/Lua/virus_scanner.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# I MAY IMPLEMENT A VIRUS SCANNER FOR THE VIRTUAL COMPUTERS
+
+# Declare member variables here. Examples:
+
+# EICAR Explanation Wikipedia - https://en.wikipedia.org/wiki/EICAR_test_file
+# EICAR Explanation (Developer's Website) - http://2016.eicar.org/86-0-Intended-use.html
+var eicar = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" # EICAR Test File - ¡¡¡NOT A REAL VIRUS!!!
+
+# GTube Explanation Wikipedia - https://en.wikipedia.org/wiki/GTUBE
+# GTube Explanation (Developer's Website) - https://spamassassin.apache.org/gtube/
+var gtube = "XJS*C4JDBQADN1.NSBN3*2IDNEN*GTUBE-STANDARD-ANTI-UBE-TEST-EMAIL*C.34X" # GTube Test Line - ¡¡¡NOT REAL SPAM!!!
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
